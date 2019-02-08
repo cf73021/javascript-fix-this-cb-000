59cbe92c0770a148910cfa3bd3591f67092e1afc
Chris Franklin <cf73021@gmail.com>
2019-02-08 19:01:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function makeCake() {
 
 function makePie() {
   var updatePieStatus = updateStatus.bind(this);
-  mix(updatePieStatus)
+  updatePieStatus("Prep")
 }
 
 function updateStatus(statusText) {
