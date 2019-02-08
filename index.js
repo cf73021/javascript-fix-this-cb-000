30d5580b53dea5053ff9e50d6a2a0d832db09ecf
Chris Franklin <cf73021@gmail.com>
2019-02-08 19:27:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,6 +60,7 @@ function mix(updateFunction) {
 
 function cool(updateFunction) {
   var status = "It has to cool! Hands off!"
+  updateFunction(status)
   setTimeout(function() {
     this.decorate(updateFunction)
   }, 2000)
